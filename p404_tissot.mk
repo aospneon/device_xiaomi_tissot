da7b740db59d98628740ce5aa0afcead02be3360
neonmicky <neonmicky30@gmail.com>
2022-05-09 11:26:06 +0000

    tissot:bringup p-404

--- p404_tissot.mk
+++ p404_tissot.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/404/configs/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
@@ -43,4 +43,4 @@ BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:u
 
 # Lineage Stuffs
 TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_QUICK_TAP := true
+#TARGET_SUPPORTS_QUICK_TAP := true
