6a4a98f19b7bd612221ae89f53de481528faca20
neonmicky <63160537+neonmicky@users.noreply.github.com>
2022-05-20 07:16:29 +0530

    tissot: Define Board Platform

--- p404_tissot.mk
+++ p404_tissot.mk
@@ -44,3 +44,6 @@ BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:u
 # Lineage Stuffs
 TARGET_GAPPS_ARCH := arm64
 #TARGET_SUPPORTS_QUICK_TAP := true
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8953
